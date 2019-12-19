88f9bb43c1b3422a9a0a58000e6a2198689185d5
Dror Dayan <dror.d.dayan@gmail.com>
2019-12-19 21:19:36 +0200

    back to original code with new is valid function

--- gen_path.py
+++ gen_path.py
@@ -1,67 +1,28 @@
 from arr2_epec_seg_ex import *
 from collision_detection import *
-from conversions import *
-import sample_polygons
 import queue
 import random
 from math import pi
+import time
 
-def rootp(s, parent):
-	if (parent[s] == s):
-		return s
-	parent[s] = rootp(parent[s], parent)
-	return parent[s]
 
-def connected(a, b, parent):
-	return rootp(a, parent) == rootp(b, parent)
-
-def connect(a, b, parent):
-	a = rootp(a, parent)
-	b = rootp(b, parent)
-	parent[a] = b
-
-def generate_milestones(p1, p2, l, polygons, epsilon, n, max_x, max_y, min_x, min_y, length):
+def generate_milestones(p1, p2, l, polygons, epsilon, n, max_x, max_y, min_x, min_y):
 	if not is_position_valid(p1[0], p1[1], p1[2], l, polygons, epsilon):
 		return []
 	if not is_position_valid(p2[0], p2[1], p2[2], l, polygons, epsilon):
 		return []
 	v = []
-
-	for theta in range(0, int(2 * pi * length.to_double()), int(epsilon.to_double())):
-		theta = FT(theta)
-		for x in range(min_x, max_x, int(epsilon.to_double())):
-			x = FT(x)
-			for y in range(min_y, max_y, int(epsilon.to_double())):
-				y = FT(y)
-				if is_position_valid(x, y, theta / length, l, polygons, epsilon):
-					if len(v) and v[-1].x() == x and v[-1].y().to_double() + int(epsilon.to_double()) == y.to_double():
-						v.remove(v[-1])
-					v.append(Point_3(x, y, theta))
-					if len(v) > n:
-						return
-		for y in range(min_y, max_y, int(epsilon.to_double())):
-			y = FT(y)
-			for x in range(min_x, max_x, int(epsilon.to_double())):
-				x = FT(x)
-				if is_position_valid(x, y, theta / length, l, polygons, epsilon):
-					if len(v) and v[-1].y() == y and v[-1].x().to_double() + int(epsilon.to_double()) == x.to_double():
-						v.remove(v[-1])
-					v.append(Point_3(x, y, theta))
-					if len(v) > n:
-						return
-		print(str(theta) + " " + str(len(v)))
-	# while len(v) < n:
-	# 	x = FT(random.randint(min_x, max_x))
-	# 	y = FT(random.randint(min_y, max_y))
-	# 	theta = FT(random.uniform(0, 2 * pi * length.to_double()))
-	# 	if is_position_valid(x, y, theta / length, l, polygons, epsilon):
-	# 		v.append(Point_3(x, y, theta))
-	# 	if not len(v) % 10000:
-	# 		print(len(v))
+	while len(v) < n:
+		x = FT(random.randint(min_x, max_x))
+		y = FT(random.randint(min_y, max_y))
+		theta = FT(random.uniform(0, 2 * pi))
+		if is_position_valid(x, y, theta, l, polygons, epsilon):
+			v.append(Point_3(x, y, theta))
 	# print(v)
 	return v
 
-def bfs(v, g, s, t, path, length):
+
+def bfs(v, g, s, t, path):
 	q = queue.Queue()
 	parent = [-1 for a in g]
 	edge = [[] for a in g]
@@ -75,12 +36,12 @@ def bfs(v, g, s, t, path, length):
 			if parent[a] == -1:
 				parent[a] = s
 				edge[a] = e
-				q.put(a)
+				q.put(s)
 	if parent[t] == -1:
 		return False
-	path.append([v[t].x(), v[t].y(), v[t].z(), True])
-	while parent[t] != t:
-		path.append([v[parent[t]].x(), v[parent[t]].y(), v[parent[t]].z() / length, not edge[t]])
+	path.append(v[t] + [True])
+	while parent[t] != -1:
+		path.append(v[parent[t]] + [not edge[t][3]])
 		t = parent[t]
 	return True
 
@@ -102,98 +63,92 @@ def k_nn(tree, k, query, eps):
 	search.k_neighbors(lst)
 	return lst
 
-def is_valid(p1, p2, epsilon, length):
-	# path_polygons = []
-	# sample_polygons.sample_polygons(segment[0], segment[1], length, path_polygons)
 
-	# path_set = Polygon_set_2()
-	# path_set.join_polygons(path_polygons)
-	# obstacles_set = Polygon_set_2()
-	# obstacles_set.join_polygons(obstacle_polygons)
+def is_valid(p1, p2, l, polygons, epsilon, clockwise):
+	p1x, p1y, p1z = p1.x(), p1.y(), p1.z()
+	p2x, p2y, p2z = p2.x(), p2.y(), p2.z()
+	curr = [p1x, p1y, p1z]
+	x_diff = p2x - p1x
+	y_diff = p2y - p1y
+	if (clockwise and p1z >= p2z) or (not clockwise and p1z < p2z):
+		z_diff = p2z - p1z
+	elif clockwise and p1z < p2z:
+		z_diff = FT(-1)*(p1z+(FT(2*pi)-p2z))
+	else:
+		z_diff = p2z+(FT(2*pi)-p1z)
+	length = math.sqrt(float(str(x_diff * x_diff + y_diff * y_diff + z_diff * z_diff)))
+	# print(length)
+	num_of_steps = math.ceil(length/(float(str(epsilon))/2))
+	diff_vec = [x_diff/FT(num_of_steps), y_diff/FT(num_of_steps), z_diff/FT(num_of_steps)]
+	# print(num_of_steps)
+	for i in range(num_of_steps):
+		curr[0] += diff_vec[0]
+		curr[1] += diff_vec[1]
+		curr[2] = FT(float(str((curr[2] + diff_vec[2] + FT(2*pi)))) % (2*pi))
+		# print(curr)
+		if not is_position_valid(curr[0], curr[1], curr[2], l, polygons, epsilon):
+			return False
+	# print(p1)
+	# print(p2)
+	return True
 
-	# #check that there are no collisions
-	# return True if not path_set.do_intersect(obstacles_set) else False
-	x = p1.x().to_double() - p2.x().to_double()
-	y = p1.y().to_double() - p2.y().to_double()
-	z = p1.z().to_double() - p2.z().to_double()
-	if (z < 0):
-		z += 2 * pi
-	z = min(z, 2 * pi - z)
-	z *= length.to_double()
-	return (x ** 2 + y ** 2 + z ** 2) ** 0.5 < 2 * epsilon.to_double()
 
-def phash(point):
-	return str([point.x(), point.y(), point.z()])
-
-def make_graph(milestones, obstacles, epsilon, length):
-	parent = [i for i in range(len(milestones))]
-	obstacle_polygons = []
-	for obs in obstacles:
-	  p = tuples_list_to_polygon_2(obs)
-	  obstacle_polygons.append(p)
-	indexing = {}
-	for i in range(len(milestones)):
-		indexing[phash(milestones[i])] = i
-
-	g = [[] for milestone in milestones]
+def make_graph(milestones, l, polygons, epsilon):
+	g = []
 	tree = Kd_tree(milestones)
-	number_of_neighbors = 5
-	for i, milestone in enumerate(milestones):
-		if not i % 10000:
-			print(i)
+	number_of_neighbors = 13
+	groups = {}
+	for milestone in milestones:
+		groups[str(milestone)] = [str(milestone)]
+	for milestone in milestones:
 		temp = []
 		nn = k_nn(tree, number_of_neighbors + 1, milestone, FT(Gmpq(0.0)))  # the + 1 to number_of_neighbors is to count for count v as it's neighbor
 		for neighbor in nn[1:]:  # first point is self and no need for edge from v to itself
-			if neighbor[1] > FT(Gmpq(0)):  # no need for edge from v to itself (just making sure)
-				j = indexing[phash(neighbor[0])]
-				if connected(i, j, parent):
-					continue
-				#print(str(neighbor[1]) + " " + str(milestones[i]) + " " + str(milestones[j]))
-				# segment = [[milestone.x(), milestone.y(), milestone.z(), True], [neighbor[0].x(), neighbor[0].y(), neighbor[0].z(), True]]
-				# if is_valid(segment, obstacle_polygons, epsilon):
-				# 	connect(i, j, parent)
-				# 	g[i].append((j, True))
-				# 	g[j].append((i, False))
-				# else:
-				# 	segment[1][3] = False
-				# 	if is_valid(segment, obstacle_polygons, epsilon):
-				# 		connect(i, j, parent)
-				# 		g[i].append((j, False))
-				# 		g[j].append((i, True))
-				if neighbor[1].to_double() < (2 * epsilon.to_double()) ** 2:
-					z = milestones[i].z().to_double() - milestones[j].z().to_double()
-					connect(i, j, parent)
-					if (z < 0):
-						z += 2 * pi * length.to_double()
-					if (z < pi * length.to_double()):
-						g[i].append((j, True))
-						g[j].append((i, False))
-					else:
-						g[i].append((j, False))
-						g[j].append((i, True))
-	print(connected(len(g) - 1, len(g) - 2, parent))
+			if groups[str(milestone)].count(str(neighbor[0])) == 0:
+				# notice: the check here should be for 2 different edges (the clockwise and counter-clockwise)
+				if is_valid(milestone, neighbor[0], l, polygons, epsilon, True):
+					temp.append((neighbor[0], True))
+					groups[str(milestone)] = groups[str(milestone)] + groups[str(neighbor[0])]
+					groups[str(neighbor[0])] = groups[str(milestone)]
+				elif is_valid(milestone, neighbor[0], l, polygons, epsilon, False):
+					temp.append((neighbor[0], False))
+					groups[str(milestone)] = groups[str(milestone)] + groups[str(neighbor[0])]
+					groups[str(neighbor[0])] = groups[str(milestone)]
+		g.append((milestone, temp))
 	return g
 
 
 def generate_path(path, length, obstacles, origin, destination):
+	print(path)
+	print(length)
+	print(obstacles)
+	print(origin)
+	print(destination)
+	start = time.time()
 	max_x, max_y, min_x, min_y = get_min_max(int(str(length)), obstacles, origin, destination)
 	poly_obstacles = [Polygon_2([Point_2(x, y) for x, y in obs]) for obs in obstacles]
 	origin = [FT(Gmpq(x)) for x in origin]
 	destination = [FT(Gmpq(x)) for x in destination]
 	my_eps = FT(5)
-	number_of_points_to_find = 1000000
+	number_of_points_to_find = 10000
 
-	milestones = generate_milestones(origin, destination, length, poly_obstacles, my_eps, number_of_points_to_find, max_x, max_y, min_x, min_y, length)
+	milestones = generate_milestones(origin, destination, length, poly_obstacles, my_eps, number_of_points_to_find, max_x, max_y, min_x, min_y)
 	if len(milestones) == 0:
 		return
-	print("hello")
 	# add origin and destination
-	milestones.append(Point_3(origin[0], origin[1], origin[2] * length))
-	milestones.append(Point_3(destination[0], destination[1], destination[2] * length))
-	print(milestones[:min(10000, len(milestones))])
-	g = make_graph(milestones, obstacles, my_eps, length)
+	milestones.append(Point_3(origin[0], origin[1], origin[2]))
+	milestones.append(Point_3(destination[0], destination[1], destination[2]))
+
+	print(milestones)
+	g = make_graph(milestones, length, poly_obstacles, my_eps)
+	print(g)
+
+	done = time.time()
+	elapsed = done - start
+	print(elapsed)
+
 	temp = []
-	success = bfs(milestones, g, len(milestones) - 1, len(milestones) - 2, temp, length)
+	success = bfs(milestones, g, len(milestones) - 1, len(milestones) - 2, temp)
 	if success:
 		for t in temp:
 			path.append(t)
